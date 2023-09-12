15c021fbc6e2c80cabae8c5314c09834e1daeec6
santiagobravin996 <santiagobravin996@gmail.com>
2023-09-12 10:16:56 -0300

    conexion DB

---
+++ README.md
@@ -0,0 +1,15 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Courts Padelea API
+
+1. Clonar proyecto
+2. ```yarn install```
+3. Configurar variables de entorno en ```.env```
+4. Levantar base de datos
+```
+docker-compose up -d
+```
+5. Levantar proyecto: 
+```yarn start:dev```
---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CourtsModule } from './courts/courts.module';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    CourtsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities:true,
+      synchronize: true
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
